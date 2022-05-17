8aab459f40661ab6a09879a6fd1dea8638d11f0a
havieta9 <havieta9@gmail.com>
2022-05-17 12:45:20 +0700

    game

--- scripts/getFilenames.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs')
-var files = fs.readdirSync('./src/sprites-copy/')
-
-files.forEach((file) => {
-  // console.log(`import img${file.replace('.png','')} from './${file}'`);
-  // console.log(`'${file.replace('.png','')}': img${file.replace('.png','')},`);
-})
